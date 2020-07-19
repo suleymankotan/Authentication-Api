fa17796280e129e7c445b439e60cd9a8b155d259
suleymankotan <suleymankotan@gmail.com>
2020-07-19 06:53:16 +0300

    initial commit

---
+++ src/test/java/com/suleyman/authenticationapi/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleyman.authenticationapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
